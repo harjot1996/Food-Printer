4a7dd9bb6531acef795810987c3daf36b647df7a
Harjot Singh <harjot1996@gmail.com>
2022-05-08 11:31:02 -0400

    new gcode generator

--- generate_gcode.py
+++ generate_gcode.py
@@ -2,95 +2,94 @@ from os import listdir
 from os.path import isfile, join
 from stl import mesh
 import stl
+import subprocess
 
 
-class SlicerShell:
+def find_min_max(obj):
+    min_x = max_x = min_y = max_y = min_z = max_z = None
+    for p in obj.points:
+        if min_x is None:
+            min_x = p[stl.Dimension.X]
+            max_x = p[stl.Dimension.X]
+            min_y = p[stl.Dimension.Y]
+            max_y = p[stl.Dimension.Y]
+            min_z = p[stl.Dimension.Z]
+            max_z = p[stl.Dimension.Z]
+        else:
+            max_x = max(p[stl.Dimension.X], max_x)
+            min_x = min(p[stl.Dimension.X], min_x)
+            max_y = max(p[stl.Dimension.Y], max_y)
+            min_y = min(p[stl.Dimension.Y], min_y)
+            max_z = max(p[stl.Dimension.Z], max_z)
+            min_z = min(p[stl.Dimension.Z], min_z)
+    return float(min_x), float(max_x), float(min_y), float(max_y), float(min_z), float(max_z)
+
+
+class GcodeGenerator:
     BED_CENTER_X = 185
     BED_CENTER_Y = 208
 
-    def __init__(self, extrusion_mults, base_offset):
-        self.extrusion_mults = extrusion_mults
+    def __init__(self, configs, base_offset):
+        self.CONFIGS = configs
+        self.EXT_MULTIPLIERS = self.generate_extrusion_multiplier_dict()
+        self.SLIC3R_PATH = '../Slic3r/slic3r.pl'
         self.BASE_OFFSET = float(base_offset)
         self.INPUT_DIR = './input'
+        self.TEMP_DIR = './temp'
 
-    def find_mins_maxs(self, obj):
-        minx = maxx = miny = maxy = minz = maxz = None
-        for p in obj.points:
-            if minx is None:
-                minx = p[stl.Dimension.X]
-                maxx = p[stl.Dimension.X]
-                miny = p[stl.Dimension.Y]
-                maxy = p[stl.Dimension.Y]
-                minz = p[stl.Dimension.Z]
-                maxz = p[stl.Dimension.Z]
-            else:
-                maxx = max(p[stl.Dimension.X], maxx)
-                minx = min(p[stl.Dimension.X], minx)
-                maxy = max(p[stl.Dimension.Y], maxy)
-                miny = min(p[stl.Dimension.Y], miny)
-                maxz = max(p[stl.Dimension.Z], maxz)
-                minz = min(p[stl.Dimension.Z], minz)
-        return float(minx), float(maxx), float(miny), float(maxy), float(minz), float(maxz)
+    def generate_extrusion_multiplier_dict(self):
+        extrusion_multipliers = {}
+        for config in self.CONFIGS:
+            extrusion_multipliers[config[0]] = config[2]
+        return extrusion_multipliers
 
-    def write_slice_shells(self):
+    def generate_gcode_metadata(self, stl_files):
         first = True
         delta_x = 0
         delta_y = 0
         delta_z = 0
+        metadata = []
+        for f in stl_files:
+            ext_multiplier = self.EXT_MULTIPLIERS[f]
+            mesh_ = mesh.Mesh.from_file(self.INPUT_DIR + '/' + f)
+            min_x, max_x, min_y, max_y, min_z, max_z = find_min_max(mesh_)
+            center_x = min_x + ((max_x - min_x) / 2)
+            center_y = min_y + ((max_y - min_y) / 2)
+            if first:
+                delta_x = self.BED_CENTER_X - center_x
+                delta_y = self.BED_CENTER_Y - center_y
+                delta_z = self.BASE_OFFSET - min_z
+            x_coord = round(center_x + delta_x, 2)
+            y_coord = round(center_y + delta_y, 2)
+            z_offset = round(min_z + delta_z, 2)
+            metadata.append([f, str(x_coord), str(y_coord), str(z_offset), str(ext_multiplier)])
+        return metadata
 
-        STL_file_path = "./STL_files/"
-        STL_files = [f for f in listdir(STL_file_path) if isfile(join(STL_file_path, f))]
+    def invoke_slicer(self, meta):
+        cmd = '--gcode-comments --infill-speed 10 --fill-density 80 --z-offset '
+        cmd += str(meta[3])  # z-offset
+        cmd += ' --skirt-height 0 --skirts 0 --print-center '
+        cmd += str(meta[1])  # print-center x coord
+        cmd += ','
+        cmd += str(meta[2])  # print-center y coord
+        cmd += ' --first-layer-speed 10 --bridge-speed 20 --perimeter-speed 10 --travel-speed 30'
+        cmd += ' --small-perimeter-speed 10 --external-perimeter-speed 10 --solid-infill-speed 20'
+        cmd += ' --infill-extrusion-width 2 --layer-height 58 --first-layer-extrusion-width 2 --extrusion-multiplier '
+        cmd += str(meta[4])  # extrusion-multiplier
+        cmd += " --external-infill-pattern 'rectilinear' --solid-infill-extrusion-width 2 --perimeters 1"
+        cmd += " --bridge-flow-ratio 2 --fill-pattern 'rectilinear' --nozzle-diameter 1.2 -o "
+        cmd += self.TEMP_DIR + ' '  # directory where the gcode file needs to be stored
+        cmd += self.INPUT_DIR + '/' + str(meta[0])  # input stl file
+        subprocess.call([self.SLIC3R_PATH, cmd])
+
+    def generate_gcode(self):
+        stl_files = [f for f in listdir(self.INPUT_DIR) if isfile(join(self.INPUT_DIR, f)) and f[-4:].upper() == ".STL"]
 
         def numeric_key(x):
-            return (int(x.split('.')[0]))
-
-        STL_files = sorted(STL_files, key=numeric_key)
-        file_details = {}
-        index = -1
-        for f in STL_files:
-            if f[-4:] == ".STL":
-                index += 1
-
-                x_coord = self.BED_CENTER_X
-                y_coord = self.BED_CENTER_Y
-                z_offset = 0
-                extrusion_mul = self.extrusion_mults[f]
-                mesh_ = mesh.Mesh.from_file(STL_file_path + f)
-                # _, cog, _ = mesh_.get_mass_properties()
-                minx, maxx, miny, maxy, minz, maxz = self.find_mins_maxs(mesh_)
-                center_x = minx + ((maxx - minx) / 2)
-                center_y = miny + ((maxy - miny) / 2)
-
-                if first == True:
-                    delta_x = self.BED_CENTER_X - center_x
-                    delta_y = self.BED_CENTER_Y - center_y
-                    delta_z = self.BASE_OFFSET - minz
-                    first = False
-                x_coord = round(center_x + delta_x, 2)
-                y_coord = round(center_y + delta_y, 2)
-                z_offset = round(minz + delta_z, 2)
-                file_details[index] = [f, str(x_coord), str(y_coord), str(z_offset), str(extrusion_mul)]
-
-        print("####### file_details ########")
-        print(file_details)
-
-        slic3r_script = open("slic3r_script.sh", "w+")
-        for key in file_details.keys():
-            slic3r_script.write(
-                "../Slic3r/slic3r.pl --gcode-comments --infill-speed 10 --fill-density 80 --z-offset ")
-            slic3r_script.write(file_details[key][3])  # z-offset
-            # slic3r_script.write("../../Slic3r/slic3r.pl --gcode-comments --infill-speed 10 --fill-density 80 ")
-            slic3r_script.write(" --skirt-height 0 --skirts 0 --print-center ")
-            slic3r_script.write(file_details[key][1])  # print-center x coord
-            slic3r_script.write(",")
-            slic3r_script.write(file_details[key][2])  # print-center y coord
-            slic3r_script.write(
-                " --first-layer-speed 10 --bridge-speed 20 --perimeter-speed 10 --travel-speed 30 --small-perimeter-speed 10 --external-perimeter-speed 10 --solid-infill-speed 20 --infill-extrusion-width 2 --layer-height 58 --first-layer-extrusion-width 2 --extrusion-multiplier ")
-            slic3r_script.write(file_details[key][4])
-            slic3r_script.write(
-                " --external-infill-pattern 'rectilinear' --solid-infill-extrusion-width 2 --perimeters 1 --bridge-flow-ratio 2 --fill-pattern 'rectilinear' --nozzle-diameter 1.2 -o ")
-            slic3r_script.write("./gcode_unclean ")
-            slic3r_script.write("./STL_files/")
-            slic3r_script.write(file_details[key][0])  # input
-            slic3r_script.write('\n')
-        slic3r_script.close()
+            return int(x.split('.')[0])
+        stl_files = sorted(stl_files, key=numeric_key)
+        metadata = self.generate_gcode_metadata(stl_files)
+        print("----- Gcode Metadata -----")
+        print(metadata)
+        for meta in metadata:
+            self.invoke_slicer(meta)
